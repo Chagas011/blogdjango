ea3af847ff6663c75ac22a83cada63f4b5a67849
Chagas011 <washington.chagas.9@hotmail.com>
2022-08-09 21:34:10 -0300

    testes de view

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
-
+from django.urls import reverse, resolve
+from views.post_index import PostIndex
 # Create your tests here.
+
+
+class PostURLSTest(TestCase):
+
+    def test_post_home_url(self):
+        url = reverse('post_index')
+        self.assertEqual(url, '/')
+
+    def test_post_categoria_url(self):
+        url = reverse('post_categoria', args=('python',))
+        self.assertEqual(url, '/categoria/python')
+
+    def test_post_busca_url(self):
+        url = reverse('post_busca')
+        self.assertEqual(url, '/busca/')
+
+    def test_post_detalhes_url(self):
+        url = reverse('post_detalhes', kwargs={'pk': 1})
+        self.assertEqual(url, '/posts/1')
+
+    def test_post_formulario_url(self):
+        url = reverse('post_forms')
+        self.assertEqual(url, '/posts/formulario')
+
+    def test_post_register_url(self):
+        url = reverse('register')
+        self.assertEqual(url, '/register')
+
+    def test_post_login_url(self):
+        url = reverse('login_form')
+        self.assertEqual(url, '/login')
+
+    def test_post_logout_url(self):
+        url = reverse('logout')
+        self.assertEqual(url, '/logout')
+
+    def test_post_dashboard_url(self):
+        url = reverse('dashboard')
+        self.assertEqual(url, '/dashboard')
+
+    def test_post_dashboard_update_url(self):
+        url = reverse('dashboard_update', kwargs={'pk': 1})
+        self.assertEqual(url, '/dashboard/post/1')
+
+
+class PostViewsTest(TestCase):
+    def test_post_home_view(self):
+        view = resolve(reverse('post_index'))
+        self.assertIs(view.func.view_class, PostIndex)
